9c2855a0a02f0a6418d0abf41b7279f7ff8bf8a5
dohun1017 <dohun10170@gmail.com>
2019-08-13 17:40:15 +0900

    여전히

--- src/main/Shape.java
+++ src/main/Shape.java
@@ -39,14 +39,16 @@ public class Shape {
 		deltaX = 0;
 		x = 4;
 		y = 0;
-		//속도 난이도 변경
-		if(this.board.getScore() >= 100) 
+
+		// 속도 난이도 변경
+		if (this.board.getScore() >= 100)
 			normal = 100;
-		else if(this.board.getScore() >= 110)
+		else if (this.board.getScore() >= 110)
 			normal = 70;
 		else
 			normal -= (this.board.getScore() / 10) * 50;
 		delay = normal;
+
 		time = 0;
 		lastTime = System.currentTimeMillis();
 		reference = new int[coords.length][coords[0].length];
@@ -56,28 +58,75 @@ public class Shape {
 	}
 
 	public void update() {
+		//바닥 움직이기 위한 변수(수정)
+		long start, end;
+		
+		// 움직 일 수 있게 함
 		moveX = true;
+
+		// ??
 		time += System.currentTimeMillis() - lastTime;
 		lastTime = System.currentTimeMillis();
 
+		// 충돌이 true이면
 		if (collision) {
 			for (int row = 0; row < coords.length; row++) {
 				for (int col = 0; col < coords[0].length; col++) {
 					if (coords[row][col] != 0)
+						// 현재 보드에 있는 쌓인 블록들과 합침
 						board.getBoard()[y + row][x + col] = color;
 				}
 			}
+			// 줄 검사(삭제)
 			checkLine();
+			// 현재 보드에 도형 최신화
 			board.setCurrentShape();
 		}
 
-		//가로축 블록 안겹치게 함, 좌우 이동
-		if (!(x + deltaX + coords[0].length > 10) && !(x + deltaX < 0)) {
-			
+		// 내려올때 블록 겹치게함 방지, 내려옴
+		// !(y(0) + 1 + 현재 도형의 y축 > 20)
+		if (!(y + 1 + coords.length > 20)) {
 			for (int row = 0; row < coords.length; row++) {
 				for (int col = 0; col < coords[row].length; col++) {
-					if (coords[row][col] != 0) {
-						//떨어지고 난뒤 안움직이게 함
+					if (coords[row][col] != 0) { // 현재 도형의 배열 중 속성이 0이 아닐 때
+						if (board.getBoard()[y + 1 + row][x + col] != 0) { // 현재 보드의[y+1+세로][x+가로]가 0이 아닐 때
+							collision = true;
+						}
+					}
+				}
+			}
+			// 내려오는 것 확인(속도에 따라 delay가 달라짐) y축 1증가
+			if (time > delay) {
+				y++;
+				time = 0;
+			}
+		}
+		
+		//다 내려 왔을 때
+		else {
+			collision = true;
+		}
+		
+		/* 바로 땅바닥에 붙지 않게 하기 위한 도전
+		//다 내려 왔을 때
+		else {
+			start = System.currentTimeMillis();
+			while (true) {
+				end = System.currentTimeMillis();
+				if (end - start >= 1000) {
+					collision = true;
+					break;
+				}
+			}
+		}*/
+
+		// 가로축 블록 안겹치게 함, 좌우 이동
+		// !(x(4) + 움직인 위치 + 움직이는 도형의 X축 > 10) 그리고 !(x(4) + 움직인 위치 < 0)
+		if (!(x + deltaX + coords[0].length > 10) && !(x + deltaX < 0)) {
+			for (int row = 0; row < coords.length; row++) {
+				for (int col = 0; col < coords[row].length; col++) {
+					if (coords[row][col] != 0) { // 현재 도형의 배열 중 속성이 0이 아닐 때
+						// 바닥 혹은 쌓인 블록에 닿은 경우 가로축 이동 안됨)
 						if (board.getBoard()[y + row][x + deltaX + col] != 0) {
 							moveX = false;
 						}
@@ -85,40 +134,22 @@ public class Shape {
 				}
 			}
 
+			// 가로축이동이 안되면 x(4) + 움직인 위치
 			if (moveX)
 				x += deltaX;
 
-		}
-		
-		//내려올때 블록 겹치게함 방지, 내려옴
-		if (!(y + 1 + coords.length > 20)) {
-			for (int row = 0; row < coords.length; row++) {
-				for (int col = 0; col < coords[row].length; col++) {
-					if (coords[row][col] != 0) { // 배열에 뭔가가 들어있을때
-						if (board.getBoard()[y + 1 + row][x + col] != 0) {
-							collision = true;
-						}
-					}
-				}
-			}
-			if (time > delay) {
-				y++;
-				time = 0;
-			}
-		} else {
-			collision = true;
 		}
 
 		deltaX = 0;
 	}
 
 	public void render(Graphics g) {
-		
+
 		// 현재 도형(움직이는 역할 X)
 		for (int row = 0; row < coords.length; row++) {
 			for (int col = 0; col < coords[0].length; col++) {
 				if (coords[row][col] != 0) {
-					g.drawImage(block, col * 30 + x * 30, row * 30 + y * 30, null);
+					g.drawImage(block, col * 30 + x * 30, row * 30 + y * 30 - 60, null);
 				}
 			}
 		}
@@ -127,10 +158,10 @@ public class Shape {
 	private void checkLine() {
 		/*
 		 * 세로(20) * 가로(10) 가로 한줄씩 검사 첫 번째 if 해당칸에 블록이 차있으면 count++ 두 번째 if count가 10보다
-		 * 작으면 size-- / count가 10보다 크거나 같으면 size--를 하지 않음 => 몇줄 없앴는지 알 수 있음
-		 * 가로줄이 꽉차있으면 그 위의 줄의 내용을 덮어 씌움
+		 * 작으면 size-- / count가 10보다 크거나 같으면 size--를 하지 않음 => 몇줄 없앴는지 알 수 있음 가로줄이 꽉차있으면 그
+		 * 위의 줄의 내용을 덮어 씌움
 		 */
-		int size = board.getBoard().length -1;
+		int size = board.getBoard().length - 1;
 		for (int i = board.getBoard().length - 1; i >= 0; i--) {
 			int count = 0;
 			for (int j = 0; j < board.getBoard()[0].length; j++) {
@@ -143,9 +174,10 @@ public class Shape {
 			}
 		}
 		if (size >= 0)
-			board.addScore(size+1);
+			board.addScore(size + 1);
 	}
 
+	// 도형 회전 메소드
 	public void rotateShape() {
 
 		if (collision)
@@ -174,7 +206,7 @@ public class Shape {
 		coords = rotatedShape;
 	}
 
-//왼쪽으로 회전
+	// 도형 배열 왼쪽 회전
 	private int[][] transposeMatrixLeft(int[][] matrix) {
 		int[][] temp = new int[matrix[0].length][matrix.length];
 		for (int i = 0; i < matrix.length; i++)
@@ -183,7 +215,7 @@ public class Shape {
 		return temp;
 	}
 
-//오른쪽으로 회전
+	// 도형 배열 오른쪽 회전
 	private int[][] transposeMatrixRight(int[][] matrix) {
 		int[][] temp = new int[matrix[0].length][matrix.length];
 		for (int i = 0; i < matrix.length; i++)
@@ -192,8 +224,17 @@ public class Shape {
 		return temp;
 	}
 
+	// 각종 게터, 세터
 	public int getColor() {
 		return color;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
 	}
 
 	public void setDeltaX(int deltaX) {
@@ -234,6 +275,10 @@ public class Shape {
 
 	public void setDirection(int direction) {
 		this.direction = direction;
+	}
+
+	public void setMoveX(boolean a) {
+		this.moveX = a;
 	}
 
 }
